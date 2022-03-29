426320a9dcc219a6d64a8e449a209b0c521315c1
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2022-03-29 23:30:00 +0000

    Add server folder

---
+++ server/bin/todart_server.dart
@@ -0,0 +1,5 @@
+import 'package:todart_server/todart_server.dart' as todart_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${todart_server.calculate()}!');
+}
---
+++ server/test/todart_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:todart_server/todart_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
