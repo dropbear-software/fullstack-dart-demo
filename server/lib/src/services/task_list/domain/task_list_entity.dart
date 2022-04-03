03968d78cd2aeff9b7c40f541c9505686060adbf
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2022-04-03 20:57:04 +0000

    Remove unneeded import

--- server/lib/src/services/task_list/domain/task_list_entity.dart
+++ server/lib/src/services/task_list/domain/task_list_entity.dart
@@ -1,6 +1,5 @@
 import 'package:built_value/built_value.dart';
 import 'package:todart_api/api_server.dart';
-import 'package:todart_server/src/extractable/uuid.dart';
 
 part 'task_list_entity.g.dart';
 
