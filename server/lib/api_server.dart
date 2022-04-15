f61837272c3e1ae6ee912e6b6aaf07871baa593a
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2022-04-15 09:48:16 +0000

    Restructure server class and add sample interceptors.

--- server/lib/api_server.dart
+++ server/lib/api_server.dart
@@ -1,25 +1,57 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:grpc/grpc.dart' as grpc;
 import 'src/infrastucture_layer.dart';
 import 'src/shared/logger.dart';
 
+final List<grpc.Service> _services = [
+  TaskListServiceHandler(),
+  TaskServiceHandler()
+];
+
+List<grpc.Interceptor> _interceptors = [
+  _ApplicationInterceptors.loggerOne,
+  _ApplicationInterceptors.loggerTwo
+];
+
 class Server {
-  static final int defaultPort = 50002;
+  late final int _portnumber;
+  late final grpc.Server _server;
+
+  Server() {
+    _server = grpc.Server(_services, _interceptors);
+    _portnumber = 50002;
+  }
 
   Future<void> start(List<String> args) async {
-    // Tell the server which services it should serve
-    final server =
-        grpc.Server([TaskListServiceHandler(), TaskServiceHandler()]);
-    await server.serve(port: defaultPort, address: '127.0.0.1');
-    log.info('Server listening on port ${server.port}...');
+    await _server.serve(port: _portnumber, address: '127.0.0.1');
+    log.info('Server listening on port ${_server.port}...');
 
     // Watch for Ctrl+C and shut down the server if needed
     ProcessSignal.sigint.watch().listen((signal) {
       if (signal == ProcessSignal.sigint) {
-        log.info("Interupt signal received, shutting down");
-        server.shutdown();
-        exit(0);
+        stop();
       }
     });
+  }
+
+  Future<void> stop() async {
+    log.info("Interupt signal received, shutting down");
+    await _server.shutdown();
+    exit(0);
+  }
+}
+
+class _ApplicationInterceptors {
+  static FutureOr<grpc.GrpcError?> loggerOne(
+      grpc.ServiceCall call, grpc.ServiceMethod method) {
+    log.info('Incoming request for ${method.name}');
+    return null;
+  }
+
+  static FutureOr<grpc.GrpcError?> loggerTwo(
+      grpc.ServiceCall call, grpc.ServiceMethod method) {
+    log.info('Call info: ${call.clientMetadata}');
+    return null;
   }
 }
