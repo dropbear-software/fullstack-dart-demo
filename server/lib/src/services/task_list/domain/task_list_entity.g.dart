7d7bc4c4b7c87cc1c2179ecc635cbb0d165e1ec9
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2022-07-22 21:26:03 +0000

    Upgrade Dart versions and dependencies

--- server/lib/src/services/task_list/domain/task_list_entity.g.dart
+++ server/lib/src/services/task_list/domain/task_list_entity.g.dart
@@ -13,12 +13,12 @@ class _$TaskListEntity extends TaskListEntity {
   final TaskList taskList;
 
   factory _$TaskListEntity([void Function(TaskListEntityBuilder)? updates]) =>
-      (TaskListEntityBuilder()..update(updates)).build();
+      (new TaskListEntityBuilder()..update(updates))._build();
 
   _$TaskListEntity._({required this.id, required this.taskList}) : super._() {
-    BuiltValueNullFieldError.checkNotNull(id, 'TaskListEntity', 'id');
+    BuiltValueNullFieldError.checkNotNull(id, r'TaskListEntity', 'id');
     BuiltValueNullFieldError.checkNotNull(
-        taskList, 'TaskListEntity', 'taskList');
+        taskList, r'TaskListEntity', 'taskList');
   }
 
   @override
@@ -26,7 +26,8 @@ class _$TaskListEntity extends TaskListEntity {
       (toBuilder()..update(updates)).build();
 
   @override
-  TaskListEntityBuilder toBuilder() => TaskListEntityBuilder()..replace(this);
+  TaskListEntityBuilder toBuilder() =>
+      new TaskListEntityBuilder()..replace(this);
 
   @override
   bool operator ==(Object other) {
@@ -43,7 +44,7 @@ class _$TaskListEntity extends TaskListEntity {
 
   @override
   String toString() {
-    return (newBuiltValueToStringHelper('TaskListEntity')
+    return (newBuiltValueToStringHelper(r'TaskListEntity')
           ..add('id', id)
           ..add('taskList', taskList))
         .toString();
@@ -86,16 +87,18 @@ class TaskListEntityBuilder
   }
 
   @override
-  _$TaskListEntity build() {
+  TaskListEntity build() => _build();
+
+  _$TaskListEntity _build() {
     final _$result = _$v ??
-        _$TaskListEntity._(
+        new _$TaskListEntity._(
             id: BuiltValueNullFieldError.checkNotNull(
-                id, 'TaskListEntity', 'id'),
+                id, r'TaskListEntity', 'id'),
             taskList: BuiltValueNullFieldError.checkNotNull(
-                taskList, 'TaskListEntity', 'taskList'));
+                taskList, r'TaskListEntity', 'taskList'));
     replace(_$result);
     return _$result;
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new,unnecessary_lambdas
